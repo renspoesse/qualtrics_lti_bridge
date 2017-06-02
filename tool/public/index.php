c9bbc3c988e6a868f9d22fe601d62273ba8e3955
renspoesse <renspoesse@gmail.com>
2017-06-02 16:51:22 +0200

    remove debig

--- tool/public/index.php
+++ tool/public/index.php
@@ -12,9 +12,9 @@ use QualtricsLTIBridge\LTI;
 
 if (Config::get("debug")) {
 
-    ini_set('display_startup_errors', 1);
-    ini_set('display_errors', 1);
-    error_reporting(-1);
+    //ini_set('display_startup_errors', 1);
+    //ini_set('display_errors', 1);
+    //error_reporting(-1);
 }
 
 /**
